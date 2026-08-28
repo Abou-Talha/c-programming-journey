2ac9fc7297cc3c2243fdf183676db861c436ae6b
Abou Talha Ansari <talhaansari6317@gmail.com>
2026-08-28 12:23:57 +0530

    Day 2: Variables and data types - int, float, char

---
+++ day_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main (){
+
+
+
+    // Integer - whole numbers
+    int age = 15;
+    
+    // Float - decimal numbers
+    float percentage = 85.5;
+    
+    // Char - single character
+    char grade = 'A';
+    
+    printf("My age is %d\n", age);
+    printf("My percentage is %f\n", percentage);
+    printf("My grade is %c\n", grade);
+
+    return 0;
+}
